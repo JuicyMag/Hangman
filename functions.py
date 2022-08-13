300595294f510c9830e8963aad5d72b424e65a3a
Magnus <magnus.herweyer@gmail.com>
2022-08-13 14:30:50 -0500

    Adds more logic to game to detect correct char guess

--- functions.py
+++ functions.py
@@ -1,12 +1,13 @@
 #this is where our helper functions will live. 
 from curses.ascii import *
 import random
+from tracemalloc import start
 
 #letters that are guessed throughout the game - will get added to as users guess
 letters_guessed = []
 
 #letters remaining that displays to users so they know what they've tried
-letters_remaining = ("a",'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+starting_letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 #NUMBER OF TURNS - CAN LATER MAKE THIS A VARIABLE USERS SELECT BUT WILL DEFAULT TO CONSTANT
 TURNS = 6
@@ -24,24 +25,45 @@ def pick_random_work(text_file):
 
 secret_word = pick_random_work('hangman.wordlist.10000.txt')
 
-
-
 #LOGICAL CHECKS
 ##Need to see if input is valid. Valid = is alphabetical, else throw error
 ###Need to see if 
 
-while TURNS > 0:
+while TURNS > 1:
     guess = input("What letter would you like to guess?")    
     if isalpha(guess):
-        print("this is a valid guess")
+        if guess in letters_guessed:
+            print('\n')
+            print("You already guessed this, pick another character!")
+            print("Letters guessed:",letters_guessed)
+            print("Available letters:",starting_letters)
+            print('\n')
+        else:
+            if guess not in secret_word:
+                print("Incorrect Guess. Remaining turns :", TURNS)
+                # print("this is a valid guess")
+                # remaining_letters = starting_letters.remove(guess)
+                letters_guessed.append(guess)
+                TURNS = TURNS - 1
+                starting_letters.remove(guess) 
 
-        if guess not in secret_word:
-            print("Incorrect Guess. Remaining turns :", TURNS)
-            TURNS = TURNS - 1
-
+                print('\n')
+                print("Incorrect guess! Turns remaining:", TURNS)
+                print("Letters guessed:",letters_guessed)
+                print("Available letters:",starting_letters)
+                print('\n')
+            
+            else:
+                letters_guessed.append(guess)
+                print("Correct guess!Turns remaining:", TURNS)
+                print("Letters guessed:",letters_guessed)
+                
+                
+                
     else:
-        print("this is not a valid guess")
-   
+        print("This is not a valid guess. You didn't have a turn used.")
+print("Game over!")
+
 
 
 
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 #this is where we will have our run function that runs the game
+#Not sure how to use this since I'm used to writing monolith main functions (like I'm current doing in functions now :))
